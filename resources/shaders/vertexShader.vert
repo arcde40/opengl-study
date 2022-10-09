afa9a431be6754181db20c2fc7ab346d228923dd
arcde40 <arcde40@gmail.com>
2022-10-09 18:25:49 +0900

    Chapter 07

--- resources/shaders/vertexShader.vert
+++ resources/shaders/vertexShader.vert
@@ -4,13 +4,16 @@ layout (location=1) in vec3 aColor;
 layout (location=2) in vec2 aTex;
 
 uniform float scale;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
 out vec3 color;
 out vec2 texCoord;
 
 void main()
 {
-  gl_Position = vec4(aPos.x + scale * aPos.x, aPos.y + scale * aPos.y, aPos.z + aPos.z * scale, 1.0);
+  gl_Position = proj * view * model * vec4(aPos, 1.0);
   color = aColor;
   texCoord = aTex;
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <GLFW/glfw3.h>
 #include <cmath>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "class/shaders.h"
 #include "class/VAO.h"
@@ -11,15 +14,56 @@
 #include "class/texture.h"
 
 GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-    -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-    0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-    0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f};
+    -0.5f,
+    0.0f,
+    0.5f,
+    0.83f,
+    0.70f,
+    0.44f,
+    0.0f,
+    0.0f,
+    -0.5f,
+    0.0f,
+    -0.5f,
+    0.83f,
+    0.70f,
+    0.44f,
+    5.0f,
+    0.0f,
+    0.5f,
+    0.0f,
+    -0.5f,
+    0.83f,
+    0.70f,
+    0.44f,
+    0.0f,
+    0.0f,
+    0.5f,
+    0.0f,
+    0.5f,
+    0.83f,
+    0.70f,
+    0.44f,
+    5.0f,
+    0.0f,
+    0.0f,
+    0.8f,
+    0.0f,
+    0.92f,
+    0.86f,
+    0.76f,
+    2.5f,
+    5.0f,
+};
 
 GLuint indices[] =
     {
         0, 1, 2,
-        0, 3, 2};
+        0, 2, 3,
+        0, 1, 4,
+        1, 2, 4,
+        2, 3, 4,
+        3, 0, 4};
 
 int main()
 {
@@ -71,7 +115,7 @@ int main()
 
   GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
-  Texture popcat("../resources/textures/popcat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+  Texture popcat("../resources/textures/brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
   popcat.texUnit(shaderProgram, "tex0", 0);
 
   // Double Buffering
@@ -79,16 +123,44 @@ int main()
   glClear(GL_COLOR_BUFFER_BIT);
   glfwSwapBuffers(window);
 
+  float rotation = 0.0f;
+  double prevTime = glfwGetTime();
+
+  glEnable(GL_DEPTH_TEST);
+
   while (!glfwWindowShouldClose(window))
   {
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     shaderProgram.Activate();
+
+    double crntTime = glfwGetTime();
+    if(crntTime - prevTime >= 1/60)
+    {
+      rotation += 0.5f;
+      prevTime = crntTime;
+    }
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 proj = glm::mat4(1.0f);
+
+    model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+    view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+    proj = glm::perspective(glm::radians(45.0f), (float)(pWidth / pHeight), 0.1f, 100.0f);
+
+    int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
     glUniform1f(uniID, 0.0f);
     popcat.Bind();
     VAO1.Bind();
-    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
 
     glfwPollEvents();
