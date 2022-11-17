d6fcc30cf8edf307afe11413487dd115bd83d1fb
arcde40 <arcde40@gmail.com>
2022-11-18 01:50:24 +0900

    Chapter 18

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,17 @@
 
 int width = 800, height = 800;
 
+float rectangleVertices[] =
+    {
+        // Coords   // texCoords
+        1.0f, -1.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f,
+        -1.0f, 1.0f, 0.0f, 1.0f,
+
+        1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        -1.0f, 1.0f, 0.0f, 1.0f};
+
 int main()
 {
 
@@ -44,6 +55,9 @@ int main()
   // Mac (With Retina Display)
 
   Shader shaderProgram("../resources/shaders/vertexShader.vert", "../resources/shaders/fragmentShader.frag");
+  Shader framebufferShader("../resources/shaders/framebuffer.vert", "../resources/shaders/framebuffer.frag");
+
+
 
   glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
   glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
@@ -53,6 +67,10 @@ int main()
   shaderProgram.Activate();
   glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
   glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+  framebufferShader.Activate();
+  glUniform1i(glGetUniformLocation(framebufferShader.ID, "screenTexture"), 0);
+
+
 
   // Double Buffering
   glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -61,25 +79,60 @@ int main()
 
   Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-  Model model("../resources/models/statue/scene.gltf");
+  Model model("../resources/models/crow/scene.gltf");
   glEnable(GL_DEPTH_TEST);
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_FRONT);
-  glFrontFace(GL_CW);
+
+  unsigned int rectVAO, rectVBO;
+  glGenVertexArrays(1, &rectVAO);
+  glGenBuffers(1, &rectVBO);
+  glBindVertexArray(rectVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, rectVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(rectangleVertices), &rectangleVertices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
 
   double prevTime = 0.0;
   double crntTime = 0.0;
   double timeDiff;
   unsigned int counter = 0;
 
+  unsigned int FBO;
+  glGenFramebuffers(1, &FBO);
+  glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+  unsigned int framebufferTexture;
+  glGenTextures(1, &framebufferTexture);
+  glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferTexture, 0);
+
+  unsigned int RBO;
+  glGenRenderbuffers(1, &RBO);
+  glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, pWidth, pHeight);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+  auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+  if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
+  {
+    std::cout << "FBO Error: " << fboStatus << std::endl;
+  }
+
   while (!glfwWindowShouldClose(window))
   {
     crntTime = glfwGetTime();
     timeDiff = crntTime - prevTime;
     counter++;
-    if(timeDiff > 1.0 / 30.0) {
-      std::string FPS = std::to_string((1.0/timeDiff) * counter);
-      std::string ms = std::to_string((timeDiff/counter) * 1000);
+    if (timeDiff > 1.0 / 30.0)
+    {
+      std::string FPS = std::to_string((1.0 / timeDiff) * counter);
+      std::string ms = std::to_string((timeDiff / counter) * 1000);
       std::string newTitle = "OpenGL - " + FPS + "FPS / " + ms + "ms";
       glfwSetWindowTitle(window, newTitle.c_str());
       prevTime = crntTime;
@@ -87,13 +140,22 @@ int main()
     }
 
 
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
 
     camera.Inputs(window);
     camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
     model.Draw(shaderProgram, camera);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    framebufferShader.Activate();
+    glBindVertexArray(rectVAO);
+    glDisable(GL_DEPTH_TEST);
+    glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glfwSwapBuffers(window);
 
