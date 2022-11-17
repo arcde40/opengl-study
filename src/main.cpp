85d6c7b34b5dbd802229b4221f59fe6abaad1dc0
arcde40 <arcde40@gmail.com>
2022-11-18 02:31:53 +0900

    Chapter 19

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,21 @@
 
 int width = 800, height = 800;
 
+float skyboxVertices[] =
+    {
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f};
+
+unsigned int skyboxIndices[] =
+    {
+        1, 2, 6, 6, 5, 1, 0, 4, 7, 7, 3, 0, 4, 5, 6, 6, 7, 4, 0, 3, 2, 2, 1, 0, 0, 1, 5, 5, 4, 0, 3, 7, 6, 6, 2, 3};
+
 int main()
 {
 
@@ -44,6 +59,7 @@ int main()
   // Mac (With Retina Display)
 
   Shader shaderProgram("../resources/shaders/vertexShader.vert", "../resources/shaders/fragmentShader.frag");
+  Shader skyboxShader("../resources/shaders/skybox.vert", "../resources/shaders/skybox.frag");
 
   glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
   glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
@@ -54,6 +70,9 @@ int main()
   glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
   glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
+  skyboxShader.Activate();
+  glUniform1i(glGetUniformLocation(skyboxShader.ID, "skybox"), 0);
+
   // Double Buffering
   glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
@@ -61,31 +80,88 @@ int main()
 
   Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-  Model model("../resources/models/statue/scene.gltf");
+  Model model("../resources/models/airplane/scene.gltf");
   glEnable(GL_DEPTH_TEST);
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_FRONT);
-  glFrontFace(GL_CW);
 
   double prevTime = 0.0;
   double crntTime = 0.0;
   double timeDiff;
   unsigned int counter = 0;
 
+  unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+  glGenVertexArrays(1, &skyboxVAO);
+  glGenBuffers(1, &skyboxVBO);
+  glGenBuffers(1, &skyboxEBO);
+  glBindVertexArray(skyboxVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+  std::string facesCubemap[6] =
+      {
+          "../resources/textures/skybox/right.jpg",
+          "../resources/textures/skybox/left.jpg",
+          "../resources/textures/skybox/top.jpg",
+          "../resources/textures/skybox/bottom.jpg",
+          "../resources/textures/skybox/front.jpg",
+          "../resources/textures/skybox/back.jpg"};
+
+  unsigned int cubemapTexture;
+  glGenTextures(1, &cubemapTexture);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+
+  for (unsigned int i = 0; i < 6; i++)
+  {
+    int width, height, nrChannels;
+    if(i == 0)   stbi_set_flip_vertically_on_load(true);
+    else   stbi_set_flip_vertically_on_load(false);
+    unsigned char *data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+      glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                   0,
+                   GL_RGB,
+                   width,
+                   height,
+                   0,
+                   GL_RGB,
+                   GL_UNSIGNED_BYTE,
+                   data);
+      stbi_image_free(data);
+    }
+    else
+    {
+      std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+      stbi_image_free(data);
+    }
+  }
+
   while (!glfwWindowShouldClose(window))
   {
     crntTime = glfwGetTime();
     timeDiff = crntTime - prevTime;
     counter++;
-    if(timeDiff > 1.0 / 30.0) {
-      std::string FPS = std::to_string((1.0/timeDiff) * counter);
-      std::string ms = std::to_string((timeDiff/counter) * 1000);
+    if (timeDiff > 1.0 / 30.0)
+    {
+      std::string FPS = std::to_string((1.0 / timeDiff) * counter);
+      std::string ms = std::to_string((timeDiff / counter) * 1000);
       std::string newTitle = "OpenGL - " + FPS + "FPS / " + ms + "ms";
       glfwSetWindowTitle(window, newTitle.c_str());
       prevTime = crntTime;
       counter = 0;
     }
-
 
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -94,6 +170,24 @@ int main()
     camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
     model.Draw(shaderProgram, camera);
+
+    glDepthFunc(GL_LEQUAL);
+
+    skyboxShader.Activate();
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+    view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+    projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+    glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+    glBindVertexArray(skyboxVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    glDepthFunc(GL_LESS);
 
     glfwSwapBuffers(window);
 
